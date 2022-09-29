b05c1f3a0d34deee11043f88c26700b2cfe73467
MellyMel Melnyk <114266459+mellymel-appdev4ever-lab@users.noreply.github.com>
2022-09-29 17:48:03 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,18 +38,8 @@ try:
     except URLError as e:
         streamlit.error()
 
- #   streamlit.write('The user entered ', fruit_choice)
-
-#import requests
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-# delete line - streamlit.text(fruityvice_response) # just writes the data to the screen 
-
-# take the json version of the response and normalize it
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output the data to the screen as a table
-#streamlit.dataframe(fruityvice_normalized)
-
 streamlit.stop()
+
 #import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
